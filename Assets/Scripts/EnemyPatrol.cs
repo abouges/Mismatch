45361f9b435c833e8f85bb725ec319c4a2ede64e
Eli Souvannarath <Kounnathum@gmail.com>
2019-12-14 21:40:49 -0500

    Update EnemyPatrol.cs

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -4,41 +4,5 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-	public Transform[] Waypoints;
-    public float speed;
-	public int curWayPoint;
-	public bool doPatrol = true;
-    public Vector3 Target;
-	public Vector3 moveDirection;
-	public Vector3 Velocity;
 
-    void update()
-	{
-        if(curWayPoint < Waypoints.Length)
-        {
-		    Target = Waypoints[curWayPoint].position;
-		    moveDirection = Target - transform.position;
-		    Velocity = rigidbody.velocity;
-		   if (moveDirection.magnitude < 1)
-		   {
-		        curWayPoint++;
-		   }
-		   else
-		   {
-		        Velocity = moveDirection.normalized * speed;
-		   }
-	    }
-        else
-		{
-			if (doPatrol)
-			{
-				curWayPoint = 0;
-			}
-			else
-			{
-				Velocity = Vector3.zero;
-			}
-		}
-		rigidbody.velocity = Velocity;
-    }
 }
