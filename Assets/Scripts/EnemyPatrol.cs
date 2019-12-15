793086c216ab056d7e3bb7bc76ef19a9f319446a
Eli Souvannarath <Kounnathum@gmail.com>
2019-12-14 21:39:27 -0500

    Waypoints(patrol)

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Transform[] Waypoints;
+    public float speed;
+	public int curWayPoint;
+	public bool doPatrol = true;
+    public Vector3 Target;
+	public Vector3 moveDirection;
+	public Vector3 Velocity;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void update()
+	{
+        if(curWayPoint < Waypoints.Length)
+        {
+		    Target = Waypoints[curWayPoint].position;
+		    moveDirection = Target - transform.position;
+		    Velocity = rigidbody.velocity;
+		   if (moveDirection.magnitude < 1)
+		   {
+		        curWayPoint++;
+		   }
+		   else
+		   {
+		        Velocity = moveDirection.normalized * speed;
+		   }
+	    }
+        else
+		{
+			if (doPatrol)
+			{
+				curWayPoint = 0;
+			}
+			else
+			{
+				Velocity = Vector3.zero;
+			}
+		}
+		rigidbody.velocity = Velocity;
     }
 }
