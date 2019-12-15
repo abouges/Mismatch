63eec0adaf09116f117595d1d7523adcb8c7d158
Kahinju <54955246+Kahinju@users.noreply.github.com>
2019-12-15 13:29:32 -0500

    Gate Keepers and Enemies

---
+++ Assets/Scripts/GuardianBehavior2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardianBehavior2 : MonoBehaviour
+{
+    private DialogueTrigger dialogue_trigger;
+    void Start()
+    {
+        dialogue_trigger = gameObject.GetComponent<DialogueTrigger>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player" && GameVariables.keyType == 2)
+        {
+            Debug.Log("Dialogue");
+            dialogue_trigger.TriggerDialogue();
+
+            GameVariables.keyType = 0;
+            Destroy(gameObject);
+            Debug.Log("Let through");
+            //SceneManager.LoadScene(1);
+            ItemUI.instance.UseItem();
+        }
+
+        if (collider.tag == "Player" && GameVariables.keyType != 2)
+        {
+            Debug.Log("Cannot let in");
+            Debug.Log("Dialogue");
+        }
+    }
+}
