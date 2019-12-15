8af16a5e3024cae1fda7cb1718018d9360680ead
Kahinju <54955246+Kahinju@users.noreply.github.com>
2019-12-15 04:19:32 -0500

    Walk Sound Implementation
    Gatekeeper sounds have been imported

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     public float move_speed = 3;
+    public AudioClip walk_audio;
 
     private Vector2 movement;
     private Rigidbody2D rb2d;
+
     private bool left;
     private bool right;
     private bool up;
     private bool down;
+    private bool is_walking;
+
+    private bool walking_audio_playing;
+
+    private AudioSource player_audio_source;
     private Animator anim;
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
         anim = gameObject.GetComponent<Animator>();
+        player_audio_source = gameObject.GetComponent<AudioSource>();
     }
 
     void Update()
     {
         PlayerAnimate();
+        audioControl();
     }
     void FixedUpdate()
     {
@@ -41,6 +50,7 @@ public class PlayerControl : MonoBehaviour
             left = false;
             up = false;
             down = false;
+            is_walking = true;
         }
         if(movement.x < 0)
         {
@@ -48,6 +58,7 @@ public class PlayerControl : MonoBehaviour
             left = true;
             up = false;
             down = false;
+            is_walking = true;
         }
         if(movement.y > 0)
         {
@@ -55,18 +66,39 @@ public class PlayerControl : MonoBehaviour
             left = false;
             up = false;
             down = true;
+            is_walking = true;
         }
         if(movement.y < 0)
         {
+            Debug.Log("Walking!");
             right = false;
             left = false;
             up = true;
             down = false;
+            is_walking = true;
+        }
+        if(movement.x == 0 && movement.y == 0)
+        {
+            is_walking = false;
         }
         anim.SetBool("up", up);
         anim.SetBool("down", down);
         anim.SetBool("right", right);
         anim.SetBool("left", left);
     }
-    
+    public void audioControl()
+    {
+        if (is_walking == true && walking_audio_playing == false)
+        {
+            player_audio_source.clip = walk_audio;
+            player_audio_source.Play();
+            walking_audio_playing = true;
+        }
+        if (is_walking == false)
+        {
+            player_audio_source.Stop();
+            walking_audio_playing = false;
+        }
+    }
+
 }
