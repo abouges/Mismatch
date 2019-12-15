c7c1aa220aafa60793a4294acddcf30e534ce364
Kahinju <54955246+Kahinju@users.noreply.github.com>
2019-12-15 03:13:45 -0500

    Player Animation and Floor Tilemap

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,12 +8,21 @@ public class PlayerControl : MonoBehaviour
 
     private Vector2 movement;
     private Rigidbody2D rb2d;
+    private bool left;
+    private bool right;
+    private bool up;
+    private bool down;
+    private Animator anim;
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    void Update()
+    {
+        PlayerAnimate();
+    }
     void FixedUpdate()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
@@ -24,19 +33,40 @@ public class PlayerControl : MonoBehaviour
     {
         rb2d.MovePosition(rb2d.position + movement * move_speed * Time.deltaTime);
     }
-    public void KeyPickup()
+    void PlayerAnimate()
     {
-
-    }
-    public void FoundCompanion()
-    {
-
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.tag == "Companion")
+        if(movement.x > 0)
         {
-            FoundCompanion();
+            right = true;
+            left = false;
+            up = false;
+            down = false;
         }
+        if(movement.x < 0)
+        {
+            right = false;
+            left = true;
+            up = false;
+            down = false;
+        }
+        if(movement.y > 0)
+        {
+            right = false;
+            left = false;
+            up = false;
+            down = true;
+        }
+        if(movement.y < 0)
+        {
+            right = false;
+            left = false;
+            up = true;
+            down = false;
+        }
+        anim.SetBool("up", up);
+        anim.SetBool("down", down);
+        anim.SetBool("right", right);
+        anim.SetBool("left", left);
     }
+    
 }
